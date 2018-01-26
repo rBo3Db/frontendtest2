f2fdcbecca953fbbc889448e15e0f013d07de8ea
rBo3Db <rBo3Db@mail.ru>
2018-01-26 15:36:07 +0400

    Signed-off-by: rBo3Db <rBo3Db@mail.ru>

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000
-app.get('/countries', (request, response) => {
+var port = process.env.PORT || 3000;
+/* app.get('/', (request, response) => {
     var mysql = require('mysql');
     var connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'fktrctq',
-    database : 'aliosha'
+    host: 'aahf5x4fkzvlmb.cgcsc13jbtcg.us-east-2.rds.amazonaws.com',
+    port: '3305',
+    user: 'rBo3Db',
+    password: 'fktrctqfktitymrf',
+    database : 'ebdb'
     });
     var me = response; 
     connection.query('SELECT * FROM countries', function(error, result, fields){
         me.send(JSON.stringify(result));
     });
-});
+}); */
 
 app.get('/', (request, response) =>{
     var fs = require("fs");
