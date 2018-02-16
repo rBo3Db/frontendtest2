147b739ffded063765d60e2b13fd6f822a954371
rBo3Db <rBo3Db@mail.ru>
2018-02-16 20:43:51 +0400

    вводим страну в инпут, выдаются её города.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var port = process.env.PORT || 3000;
         {
             me.send(JSON.stringify(error));
             return;
-        }
+        } 
         
         me.send(JSON.stringify(result));
     });
@@ -41,42 +41,42 @@ app.get('/getCitiesByIdOfCountry', (request, response) =>{
     host: 'countriesdb.cgcsc13jbtcg.us-east-2.rds.amazonaws.com',
     port: '3306',
     user: 'masterUser',
-    password: 'QWer123!',
+    password: 'masterpassword',
     database : 'countriesDB'});
     var citiesByIdOfCountry = response; 
     var partOfName = request.query.partOfName;
     var pageNumber = Number(request.query.pageNumber);
     pageNumber = pageNumber * 5;
 //    проверка на неправильные символы
-        connection.query ("SELECT ID FROM countries where name like '%"+partOfName+"%'", function(error, res, fields) {
-            var howManyCountries = res.length;
-            if (howManyCountries == 0) {
-                citiesByIdOfCountry.send(JSON.stringify(error));
-                return;
-            } else {
-                for (var numberOfCountries = 0; numberOfCountries < howManyCountries; numberOfCountries++) { //генератор запроса в зависимости от совпадений символов в запросе
-                    var idhui = res[numberOfCountries].ID;
-                    if(numberOfCountries == 0) {
-                    var req = 'WHERE countryID = '+ idhui; 
-                    } else {
-                        req = req + ' OR countryID = ' + idhui;
+    connection.query ("SELECT ID FROM countries where name like '%"+partOfName+"%'", function(error, res, fields) {
+        var howManyCountries = res.length;
+        if (howManyCountries == 0) {
+            citiesByIdOfCountry.send(JSON.stringify(error));
+            return;
+        } else {
+            for (var numberOfCountries = 0; numberOfCountries < howManyCountries; numberOfCountries++) { //генератор запроса в зависимости от совпадений символов в запросе
+                var idhui = res[numberOfCountries].ID;
+                if(numberOfCountries == 0) {
+                var req = 'WHERE countryID = '+ idhui; 
+                } else {
+                    req = req + ' OR countryID = ' + idhui;
+                }
+            };
+            connection.query ('SELECT Count(*) as col FROM cities ' + req, function(error, result, fields)
+            {
+                var col = result[0].col;
+                connection.query('SELECT name FROM cities ' + req +' LIMIT '+pageNumber+ ',5', function(error, data, fields){
+                    if(error)
+                    {
+                        citiesByIdOfCountry.send(JSON.stringify(error));
+                        return;    
                     }
-                };
-                connection.query ('SELECT Count(*) as col FROM cities ' + req, function(error, result, fields)
-                {
-                    var col = result[0].col;
-                    connection.query('SELECT name FROM cities ' + req +' LIMIT '+pageNumber+ ',5', function(error, data, fields){
-                        if(error)
-                        {
-                            citiesByIdOfCountry.send(JSON.stringify(error));
-                            return;    
-                        }
-                        var result = {count : col, cities : data};
-                        citiesByIdOfCountry.send(JSON.stringify(result));
-                    });
+                    var result = {count : col, cities : data};
+                    citiesByIdOfCountry.send(JSON.stringify(result));
                 });
-            };           
-        });
+            });
+        };           
+    });
 });
 
 app.listen(port, (err) => {
